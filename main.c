2039f6965119fa00145566905f926c22079f9de4
lleticiasilvaa <leticia.silva1@ufv.br>
2022-11-12 12:08:37 -0300

    procurando soma mínima

--- main.c
+++ main.c
@@ -53,6 +53,50 @@ int main(int argc, char **argv)
 
             printaMatriz(matriz,numLinhas,numColunas);
 
+            //achar soma mínima (Germano falou que essa parte é mais fácil fazendo iterativo)
+
+            int i=numLinhas-1;
+            int j=numColunas-1;
+            //começar da posição onde quer chegar => de tras pra fernte
+            //se desceria, olho pra cima
+            //se iria pra direita olho pela esquerda
+
+            //printf("\nDo fim pro inicio:");
+
+            while(true){
+                if ((i==0)&&(j==0)){ //se chegar na posição[0][0] = "inicio" do caminho onde está a soma minima
+                    printf("\n\nSoma Minima : %d",matriz[i][j].valor);
+                    break;
+                }
+
+                else if ((i!=0)&&(j==0)){ //se ainda não esta na primeira mas esta na primeira coluna
+                    //só posso olhar esquerda
+                    matriz[i-1][j].valor += matriz[i][j].valor; //incrementa valor na posição a esquerda 
+                    i--; //decrementa i para passar a olhar a partir da posição a esquerda
+                }
+
+                else if ((i==0)&&(j!=0)){ //se esta na primeira linha mas ainda nao esta na primeira coluna
+                    //só posso olhar pra cima
+                    matriz[i][j-1].valor += matriz[i][j].valor; //incrementa valor na posição a cima 
+                    j--; //decrementa j para passar a olhar a partir da posição a cima
+                }
+                
+                else { //se nao esta na nem primeira linha nem na ultima coluna
+                    //olhar pra cima e pra esquerda
+                    //compara valor //E SE FOR IGUAL? INDICOU DE TER MAIS DE UM CAMINHO COM A MESMA SOMA
+                    //e pega menor
+
+                    if ((matriz[i-1][j].valor)>=(matriz[i][j-1].valor)){ //se a cima maior => pegar a esquerda
+                        matriz[i][j-1].valor += matriz[i][j].valor;
+                        j --;
+                    }
+                    else{
+                        matriz[i-1][j].valor += matriz[i][j].valor; //se a baixo maior => pegar a cima
+                        i --;
+                    }
+                }
+            }
+
             break;
 
         default:
